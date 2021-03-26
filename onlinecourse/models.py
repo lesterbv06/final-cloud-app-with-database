c39dc4347a8a0a909dd39829d3db17902256483d
Lester06 <bloodshot_x@hotmail.com>
2021-03-26 18:15:00 +0000

    adding code for views

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -127,7 +127,7 @@ class Question(models.Model):
     # Indicate if this choice of the question is a correct one or not
     # Other fields and methods you would like to design
 class Choice(models.Model):
-    choice_text = models.CharField(max_length=20, default="choice_text")
+    choice_text = models.CharField(max_length=50, default="choice_text")
     is_correct = models.BooleanField(default=False)
     question_id = models.ManyToManyField(Question)
    
